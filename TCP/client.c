4f3e7d3fe6eb498f1953d11093b78ee6e679eb1d
Rudra Pratap Singh <rudrapratapsingh2026@cs.ajce.in>
2025-04-07 02:14:00 +0530

    gitignore added

--- TCP/client.c
+++ TCP/client.c
@@ -2,41 +2,61 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include<unistd.h>
+#include <unistd.h>
+
 int main(void)
 {
-int socket_desc;
-struct sockaddr_in server_addr;
-char server_message[2000], client_message[2000];
-memset(server_message,'\0',sizeof(server_message));
-memset(client_message,'\0',sizeof(client_message));
-socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-if(socket_desc < 0){
-printf("Unable to create socket\n");
-return -1;
-}
-printf("Socket created successfully\n");
-server_addr.sin_family = AF_INET;
-server_addr.sin_port = htons(2000);
-server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-if(connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr
-)) < 0){
-printf("Unable to connect\n");
-return -1;
-}
-printf("Connected with server successfully\n");
-printf("Enter message: ");
-gets(client_message);
-if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
-printf("Unable to send message\n");
-return -1;
-}
-if(recv(socket_desc, server_message, sizeof(server_message), 0) < 0){
-printf("Error while receiving server's msg\n");
-return -1;
-}
-printf("Server's response: %s\n",server_message);
-close(socket_desc);
-return 0;
-}
+    int socket_desc;
+    struct sockaddr_in server_addr;
+    char server_message[2000], client_message[2000];
+    
+    // Initialize buffers
+    memset(server_message, '\0', sizeof(server_message));
+    memset(client_message, '\0', sizeof(client_message));
 
+    // Create socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc < 0) {
+        printf("Unable to create socket\n");
+        return -1;
+    }
+    printf("Socket created successfully\n");
+
+    // Set up server address structure
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(2000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Connect to server
+    if (connect(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Unable to connect\n");
+        return -1;
+    }
+    printf("Connected with server successfully\n");
+
+    // Get message from user
+    printf("Enter message: ");
+    fgets(client_message, sizeof(client_message), stdin);
+
+    // Remove the newline character from the input, if any
+    client_message[strcspn(client_message, "\n")] = '\0';
+
+    // Send message to server
+    if (send(socket_desc, client_message, strlen(client_message), 0) < 0) {
+        printf("Unable to send message\n");
+        return -1;
+    }
+
+    // Receive response from server
+    if (recv(socket_desc, server_message, sizeof(server_message), 0) < 0) {
+        printf("Error while receiving server's message\n");
+        return -1;
+    }
+    
+    printf("Server's response: %s\n", server_message);
+
+    // Close the socket
+    close(socket_desc);
+
+    return 0;
+}
